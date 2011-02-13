b36276487e454140d3849ef7c0446ba3af0580bb
Meleneth <meleneth@gmail.com>
2011-02-13 14:14:42 -0800

    Initial import of blank rails 3.0.4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mediacore::Application.config.secret_token = 'b7f61fbbdbb249904b2297a5ea9e71790bee96e868683801a130631f9faf1701db267d41a3267f817a59c915cb1de5c944fa7aeaa73d14beac5afc68c1da0779'
