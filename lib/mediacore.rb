282829596ecfa4fbc574bb6c8e17883fe549fb1d
Meleneth <meleneth@gmail.com>
2011-02-16 00:04:25 -0800

    Initial split of mediacore code and the script that calls it.  Test exists too.  No content.

---
+++ lib/mediacore.rb
@@ -0,0 +1,64 @@
+
+require 'find'
+require 'rexml/document'
+require 'digest/sha1'
+
+def get_mediacore_dir()
+    directory = Dir.getwd
+    while 1
+        possible_directory = "#{directory}/.mediacore"
+        return possible_directory if File.directory? possible_directory
+        if directory == '/'
+            puts "Unable to find MediaCore directory under #{Dir.getwd}"
+            exit
+        end
+        directory = File.dirname(directory)
+    end
+end
+
+def mediacore_init()
+    Dir.mkdir('.mediacore')
+end
+
+def mediacore_status()
+    mediacore_dir = get_mediacore_dir()
+    puts "MediaCore directory is #{mediacore_dir}"
+end
+
+def mediacore_import()
+    puts "MediaCore importing hashes"
+    mediacore_dir = get_mediacore_dir()
+    
+    mediacore_hash_xml_filename = "#{mediacore_dir}/file_hashes.xml"
+    xml_doc = REXML::Document.new
+    xml_doc << REXML::XMLDecl.new
+    root_node = xml_doc.add_element('files')
+    Find.find('.') do |path|
+        if FileTest.directory?(path)
+             next if path == "."
+             Find.prune if File.basename(path) =~ /^\./
+        else
+            node = root_node.add_element('file')
+            node.attributes['path'] = File.dirname(path)
+            node.attributes['size'] = File.size(path).to_s
+            node.attributes['sha1'] = Digest::SHA1.file(path).hexdigest
+            node.attributes['name'] = File.basename(path)
+        end
+    end
+    file = File.new(mediacore_hash_xml_filename, "w+")
+    xml_doc.write(file, 0)
+end
+
+def mediacore_check()
+   mediacore_dir = get_mediacore_dir()
+
+   mediacore_hash_xml_filename = "#{mediacore_dir}/file_hashes.xml"
+   puts "#{mediacore_hash_xml_filename} loading.."
+   file = File.new mediacore_hash_xml_filename
+   xml_doc = REXML::Document.new file
+   file_count = 0
+   xml_doc.elements.each("/files/file") { |e| file_count = file_count + 1 }
+   puts "check: #{file_count}"
+
+end
+
---
+++ test/integration/mediacore_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+require 'mediacore'
+
+class MediacoreTest < ActionDispatch::IntegrationTest
+  fixtures :all
+
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
