1ab18eb4e33edfb308cdf0ba78c3026d79c78edc
Meleneth <meleneth@gmail.com>
2011-03-02 00:21:52 -0800

    Update to rails 3.0.5 generated files

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mediacore::Application.config.secret_token = 'b7f61fbbdbb249904b2297a5ea9e71790bee96e868683801a130631f9faf1701db267d41a3267f817a59c915cb1de5c944fa7aeaa73d14beac5afc68c1da0779'
+Mediacore::Application.config.secret_token = '85785152bcc1142879411d9694327914820073885f047860331d7abe8aff3ec419129dcfce0a2bcfd0ee33fdf1a7d36f6a75101e8d89616aab73777115d512d4'
